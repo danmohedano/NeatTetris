dbbd510583173154805ed77e5b9cbfa6600f180a
Daniel Mohedano <danimohedano1998@gmail.com>
2022-03-18 13:03:44 +0100

    NEAT execution

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import neat
+
 import neattetris.gamestates
 from neattetris import *
 from neat.nn import FeedForwardNetwork
 import numpy as np
 import sys
 import select
+import os
 
 
 class HumanActivation():
@@ -26,12 +29,42 @@ class HumanActivation():
         return response
 
 
-def main():
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        state = neattetris.gamestates.GameStateTetris(8, 10)
+        sim = neattetris.simulator.Simulator()
+        genome.fitness = sim.simulation(net, state)
+
+
+def main_human():
     state = neattetris.gamestates.GameStateTetris()
     sim = neattetris.simulator.Simulator()
 
     print(sim.simulation(HumanActivation(), state, True))
 
 
+def main_nn():
+    # Load configuration
+    config_path = os.path.join(os.path.dirname(__file__), 'config')
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+
+    # Create population
+    p = neat.Population(config)
+
+    # Add stdout reporter to show progress in the terminal
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    # Run 2000 generations
+    winner = p.run(eval_genomes, 2000)
+
+
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 1:
+        main_nn()
+    elif sys.argv[1] == 'human':
+        main_human()
